f78e1fc9a083e7607dab2cf1a30bdee885df0757
Ganp-YW <gerardowateyma@gmail.com>
2026-07-30 21:16:31 -0400

    Proyecto Finalizado JS

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
-function formatName(firstName, lastName) {
-    if (typeof firstName !== "string" || typeof lastName !== "string") {
-        throw new Error("Tipo de dato invalido.");
-    }
-    return `${firstName} ${lastName}`;
-}
-
-function getGreeting(timeOfDay) {
-    if (typeof timeOfDay !== "string") {
-        throw new Error("Tipo de dato invalido.");
+    function formatName(firstName, lastName) {
+        if (typeof firstName !== "string" || typeof lastName !== "string") {
+            throw new Error("Tipo de dato invalido.");
+        }
+        return `${firstName} ${lastName}`;
     }
 
-    switch (timeOfDay.toLowerCase()) {
-        case 'morning':
-            return "Good morning";
-        case 'afternoon':
-            return "Good afternoon";
-        case 'evening':
-            return "Good evening";
-        default:
-            throw new Error("Valor no admitido.");
-    }
-}
+    function getGreeting(timeOfDay) {
+        if (typeof timeOfDay !== "string") {
+            throw new Error("Tipo de dato invalido.");
+        }
 
-function createGreeting(firstName, lastName, timeOfDay) {
-    try {
-        return `${formatName(firstName, lastName)}, ${getGreeting(timeOfDay)}`;
-    } catch (error) {
-        return error.message;
+        switch (timeOfDay.toLowerCase()) {
+            case 'morning':
+                return "Good morning";
+            case 'afternoon':
+                return "Good afternoon";
+            case 'evening':
+                return "Good evening";
+            default:
+                throw new Error("Valor no admitido.");
+        }
     }
-}
+
+    function createGreeting(firstName, lastName, timeOfDay) {
+        try {
+            return `${getGreeting(timeOfDay)}, ${formatName(firstName, lastName)}`;
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    console.log(createGreeting('Ava', 'Stone', 'morning'));
+    console.log(createGreeting('Noah', 'Kim', 'evening'));
+    console.log(createGreeting('Mina', 'Patel', 'afternoon'));
